d1dba8501af6c620a8b2446db31d70562eb5034a
cocolacre <cocolacre@gmail.com>
2024-02-16 17:57:16 +0300

    initial commit

---
+++ URLs.py
@@ -0,0 +1,2 @@
+
+MAIN_PAGE_URL = 'https://en.wikipedia.org/wiki/Main_Page'
---
+++ locators.py
@@ -0,0 +1,27 @@
+
+
+MAIN_PAGE_LOGO_CSS_SELECTOR = '.mw-logo'
+MAIN_PAGE_MAIN_PAGE_LINK_CSS_SELECTOR = '#ca-nstab-main > a > span'
+MAIN_PAGE_TALK_LINK_CSS_SELECTOR = '#ca-talk > a > span'
+
+MAIN_PAGE_MAIN_MENU_DROPDOWN_CSS_SELECTOR = '#vector-main-menu-dropdown-checkbox'
+MAIN_PAGE_POWERED_BY_CSS_SELECTOR = '#footer-poweredbyico'
+MAIN_PAGE_COPYRIGHT_ICO_CSS_SELECTOR = '#footer-copyrightico'
+
+#articlecount > a:nth-child(1)
+#//*[@id="articlecount"]/a[1]
+#<a href="/wiki/Special:Statistics" title="Special:Statistics">6,784,656</a>
+# 6,784,659 
+#
+#x = $('#articlecount > a')[0].text
+# RU wiki: '.main-top-articleCount'
+
+
+LOGIN_BUTTON_CSS_SELECTOR = '#pt-login-2 > a > span'
+
+
+
+//*[@id="pt-login-2"]/a/span
+
+
+
